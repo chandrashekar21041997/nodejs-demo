ef20740b22fb975df863846a6d784125cfd7baea
chandrashekar21041997 <119240540+chandrashekar21041997@users.noreply.github.com>
2023-03-10 20:56:34 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,3 +1,5 @@
 DOCKER_USER="chandrashekar117"
 DOCKER_PASS="K.chandu@143"
 echo $DOCKER_PASS | docker login --username $DOCKER_USER --password-stdin
+          sh 'docker tag chandrashekark117/nodeapp:$BUILD_NUMBER chandrashekark117/nodeapp'
+          sh 'docker push chandrashekark117/nodeapp:$BUILD_NUMBER'
